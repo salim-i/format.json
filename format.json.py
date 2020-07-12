a2d7847fbac37e7cc4ae7c7f8966a8cabc1daebf
Salim <65913524+salim-i@users.noreply.github.com>
2020-07-12 12:41:20 +0500

    решение без counter

--- format.json.py
+++ format.json.py
@@ -1,16 +1,29 @@
 import json
-from collections import Counter
 
 with open("newsafr.json") as f:
     json_data = json.load(f)
 
 words = []
+
 for value in json_data["rss"]["channel"]["items"]:
     description = value["description"]
     news = description.lower().split()
     for word in news:
         if len(word) > 6:
             words.append(word)
-            words.sort()
-            c = Counter(words)
-print(c.most_common(10))
+
+word_count = {}
+for item in words:
+  if item not in word_count:
+    word_count[item] = 1
+  else: 
+    word_count[item] += 1
+
+my_words = []
+
+for keys, values in word_count.items():
+    my_words.append((values,keys))
+    my_words.sort(reverse = True)
+
+for key, val in my_words[:10]:
+  print (key, val)
